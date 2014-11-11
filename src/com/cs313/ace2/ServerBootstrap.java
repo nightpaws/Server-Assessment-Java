f7819ae633e19a7b6d12a32ae9800172203f86b8
nightpaws <nightpaws@Nightpawss-MacBook-Pro.local>
2014-11-11 15:03:57 +0000

    Implemented Threading
    Threading implemented, code reordered so that this is possible, however
    threads currently do not behave correctly.

--- src/com/cs313/ace2/ServerBootstrap.java
+++ src/com/cs313/ace2/ServerBootstrap.java
@@ -1,13 +1,26 @@
 package com.cs313.ace2;
 
-import java.util.concurrent.Executors;
+import java.util.Scanner;
 
 public class ServerBootstrap {
 
 	public static void main(String[] args) {
-		Thread worker = new Thread(new Server());
-		worker.start();
+		Scanner in = new Scanner(System.in);
+		Server s = new Server();
+		char input = 'n';
+		new Thread(s).start();
 
+		while (true) {
+			System.out.println("Quit? y|n");
+			input = in.nextLine().charAt(0);
+			switch (input) {
+			case 'y':
+				in.close();
+				System.exit(0);
+				break;
+			default:
+				;
+			}
+		}
 	}
-
 }
