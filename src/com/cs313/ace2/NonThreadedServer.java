fe5a62cd382d1185ea0fb4b4cb94e57b0ea25dee
Nightpaws <nightpaws@Nightpawss-MacBook-Pro.local>
2014-11-19 16:29:03 +0000

    ThreadPooling Implemented
    Thread pooling with a cached thread pool seems to be working, however
    I’ve had to kill off my user interface to allow for the server to
    listen for client input. Lots of lovely //comments in this version as a
    result

--- src/com/cs313/ace2/NonThreadedServer.java
+++ src/com/cs313/ace2/NonThreadedServer.java
@@ -4,53 +4,44 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Server implements Runnable {
-	protected Socket client;
 
-	public Server(Socket client) {
-		this.client = client;
-	}
+public class NonThreadedServer {
+	public static void main(String[] args) {
 
-	@Override
-	public void run() {
-//		while (true) {
-			try {
-				// ServerSocket sock = new ServerSocket(6100);
-				// System.out.println("Server is running!");
+		try {
+			ServerSocket sock = new ServerSocket(6100);
+			System.out.println("Server is running!");
+			// now listen for connections
+			while (true) {
 				String userInput = "";
-//				 Socket client = sock.accept();
+				Socket client = sock.accept();
 
 				// await string from client and read in from buffer
 				userInput = getInput(client, userInput);
-//				Thread.yield();
 
 				// create message to return to user
 				Message clientReturn = new MessageImpl(userInput);
 				clientReturn.setCounts();
+
 				// Check value to be returned on server
 				DebugInputCheck(userInput, clientReturn);
-//				Thread.yield();
+
 				// return message to user
 				output(client, clientReturn);
 
 				// Remember to close the stream
 				// client.close();
-				// sock.close();
 				// System.out.println("Stream has been closed");
-				if (Thread.currentThread().isInterrupted()) {
-					System.out.println("I'm interrupted!");
-//					break;
-				}
-			} catch (IOException ioe) {
-				System.err.println(ioe);
 			}
-//		}
-
+		} catch (IOException ioe) {
+			System.err.println(ioe);
+		}
 	}
 
-	private static String getInput(Socket client, String userInput)
+	public static String getInput(Socket client, String userInput)
 			throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(
 				client.getInputStream()));
@@ -59,7 +50,7 @@ public class Server implements Runnable {
 		return userInput;
 	}
 
-	private static void DebugInputCheck(String userInput, Message clientReturn) {
+	public static void DebugInputCheck(String userInput, Message clientReturn) {
 		System.out
 				.println("Message to be returned has been generated as follows");
 		System.out.println("- User Input was: " + userInput);
@@ -69,7 +60,7 @@ public class Server implements Runnable {
 
 	}
 
-	private static void output(Socket client, Message clientReturn)
+	public static void output(Socket client, Message clientReturn)
 			throws IOException {
 		ObjectOutputStream outStream = new ObjectOutputStream(
 				client.getOutputStream());
--- src/com/cs313/ace2/ServerBootstrap.java
+++ src/com/cs313/ace2/ServerBootstrap.java
@@ -1,27 +1,45 @@
 package com.cs313.ace2;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ServerBootstrap {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		Server s = new Server();
-		char input = 'n';
-		new Thread(s).start();
+		// Server s = new Server();
+//		char input = 'n';
+		// new Thread(s).start();
 
-		while (true) {
-			System.out.println("To quit, press q, then enter at any time:");
-			input = in.nextLine().charAt(0);
-			switch (input) {
-			case 'q':
-				in.close();
-				System.out.println("Terminating Server...");
-				System.exit(0);
-				break;
-			default:
-				;
+		// from server.java
+		try {
+			ServerSocket sock = new ServerSocket(6100);
+			System.out.println("Server is running!");
+			ExecutorService executorService = Executors.newCachedThreadPool();
+			while (true) {
+				Socket client = sock.accept();
+				executorService.execute(new Server(client));
+				System.out.println("To quit, press q, then enter at any time:");
+//				input = in.nextLine().charAt(0);
+//				switch (input) {
+//				case 'q':
+//					client.close();
+//					sock.close();
+//					in.close();
+//					executorService.shutdown();
+//					System.out.println("Terminating Server...");
+//					System.exit(0);
+//					break;
+//				default:
+//					;
+//				}
 			}
+		} catch (IOException ioe) {
+
 		}
 	}
 }
